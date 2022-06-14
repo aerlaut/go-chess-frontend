59c804b78a344002f8c137a72a42b769d3f49e79
Anugerah Erlaut <aerlaut@live.com>
2022-06-14 23:08:38 +0100

    setup board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { STARTING_POSITIONS } from '../constants';
 
-const numRows = 8;
-const numCols = 8;
-const cells = Array(numRows * numCols).fill();
+const NUM_ROWS = 8;
+const NUM_COLS = 8;
+
+function resetBoard() {
+  const emptyBoard = Array(NUM_ROWS * NUM_COLS).fill();
+
+  Object.entries(STARTING_POSITIONS).forEach(([position, piece]) => {
+    emptyBoard[position] = piece;
+  });
+
+  return emptyBoard;
+}
 
 function Board() {
+  const [cells] = useState(resetBoard());
+
   return (
     <div id="app">
       <h1>Go Chess</h1>
       <div id="board">
         {
-            Array(numRows).fill().map((_1, rowIndex) => (
-              Array(numCols).fill().map((_2, colIndex) => (
+            Array(NUM_ROWS).fill().map((_1, rowIndex) => (
+              Array(NUM_COLS).fill().map((_2, colIndex) => (
                 <div
                   className="board-cell"
                   key={`cell-${rowIndex}-${colIndex}`}
-                  style={{ backgroundColor: (rowIndex + colIndex) % 2 === 0 ? 'white' : 'black' }}
+                  style={{ backgroundColor: (rowIndex + colIndex) % 2 === 0 ? 'white' : '#ccc' }}
                 >
-                  {cells[rowIndex * numRows + colIndex]}
+                  {cells[rowIndex * NUM_ROWS + colIndex]}
                 </div>
               ))
             ))
